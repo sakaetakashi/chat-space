8df6b15489f26e1d0d2e0a05276be952fa970edf
sakaetakashi <komori.takasy@gmail.com>
2020-05-10 12:28:49 +0900

    非同期自動更新機能の実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -56,10 +56,45 @@ $(function () {
         $(".messages__list").animate({
           scrollTop: $(".messages__list")[0].scrollHeight,
         });
-        $("#form__submit").prop("disabled", false);
+        $("form__submit").prop("disabled", false);
       })
       .fail(function () {
         alert("メッセージ送信に失敗しました");
       });
   });
+  var reloadMessages = function () {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    var last_message_id = $(".message:last").data("message-id");
+    $.ajax({
+      //ルーティングで設定した通りのURLを指定
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: "get",
+      dataType: "json",
+      //dataオプションでリクエストに値を含める
+      data: { id: last_message_id },
+    })
+      .done(function (messages) {
+        if (messages.length !== 0) {
+          //追加するHTMLの入れ物を作る
+          var insertHTML = "";
+          //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+          $.each(messages, function (i, message) {
+            insertHTML += buildHTML(message);
+          });
+          //メッセージが入ったHTMLに、入れ物ごと追加
+          $(".messages__list").append(insertHTML);
+          $(".messages__list").animate({
+            scrollTop: $(".messages__list")[0].scrollHeight,
+          });
+        }
+      })
+      .fail(function () {
+        alert("error");
+      });
+  };
+  //$(function(){});の閉じタグの直上(処理の最後)に以下のように追記
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
