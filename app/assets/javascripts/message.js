544819283962957cddc2ab5891d40481d5232d15
sakaetakashi <komori.takasy@gmail.com>
2020-05-10 12:40:01 +0900

    非同期通信及び自動更新機能の実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -93,7 +93,6 @@ $(function () {
         alert("error");
       });
   };
-  //$(function(){});の閉じタグの直上(処理の最後)に以下のように追記
   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
     setInterval(reloadMessages, 7000);
   }
